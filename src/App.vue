183e6553a04f42ea0e6e929cb041e45337d4b138
Reza <i.am.reza.programmer@gmail.com>
2024-05-25 23:17:49 +0330

    create header

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,7 @@
     <router-view />
   </div>
 </template>
+
+<style>
+@import url(@/assets/css/typography/typographies.css);
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,15 @@
         <img src="/images/logo.jpg" alt="logo" class="logo-image">
       </div>
     </v-app-bar>
+    <v-main class="pa-5 bg-background d-flex align-center justify-center" min-height="300px" style="margin-top: 80px">
+      <div class="d-flex flex-column align-center justify-center text-center ga-3" style="max-width: 600px">
+        <h1 class="heading-h1 text-primary">گلاب کاشان</h1>
+        <h4 class="heading-h4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h4>
+        <v-btn color="primary">
+          <p class="heading-h5 pt-1 px-6 text-white"> ادامه</p>
+        </v-btn>
+      </div>
+    </v-main>
   </v-layout>
 </template>
 
