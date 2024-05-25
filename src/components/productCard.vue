88a7a1d10872f75e934b93d23ad5e6f870f0ab4b
Reza <i.am.reza.programmer@gmail.com>
2024-05-25 23:57:45 +0330

    add product card

---
+++ src/components/productCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card
+      class="mx-auto bg-background"
+      max-width="344"
+  >
+    <v-img
+        height="200px"
+        :src="image"
+        cover
+    />
+
+    <v-card-title class="heading-h2 text-end">
+      {{title}}
+    </v-card-title>
+
+    <v-card-subtitle class="heading-h5 text-end">
+      قیمت : {{price}}
+    </v-card-subtitle>
+
+    <v-card-actions dir="rtl">
+      <v-btn>
+        <p class="body-b1 text-primary">توضیحات</p>
+      </v-btn>
+
+      <v-spacer/>
+
+      <v-btn
+          :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          @click="show = !show"
+      />
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider/>
+
+        <v-card-text class="text-end body-b1">
+          {{caption}}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import {ref , type Ref } from "vue"
+
+defineProps({
+  productId: Number,
+  image: String,
+  title: String,
+  caption: String,
+  price: String
+})
+
+const show : Ref<boolean> = ref(false);
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,31 @@
 <template>
-  <v-layout>
-    <v-app-bar class=" h-auto app-bar">
-      <div class="mx-auto h-100">
-        <img src="/images/logo.png" alt="logo" class="logo-image">
-      </div>
-    </v-app-bar>
-    <v-main class="pa-5 bg-background d-flex align-center justify-center" min-height="400px" style="margin-top: 80px">
-      <div class="d-flex flex-column align-center justify-center text-center ga-3" style="max-width: 600px">
-        <h1 class="heading-h1 text-primary">گلاب افتخار</h1>
-        <h4 class="heading-h4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h4>
-        <v-btn color="primary" class="header-btn">
-          <p class="heading-h5 pt-1 px-6 text-white"> ادامه</p>
-        </v-btn>
-      </div>
-    </v-main>
-  </v-layout>
+  <div>
+    <v-layout>
+      <v-app-bar class=" h-auto app-bar">
+        <div class="mx-auto h-100">
+          <img src="/images/logo.png" alt="logo" class="logo-image">
+        </div>
+      </v-app-bar>
+      <v-main class="pa-5 bg-background d-flex align-center justify-center" min-height="400px" style="margin-top: 80px">
+        <div class="d-flex flex-column align-center justify-center text-center ga-3" style="max-width: 600px">
+          <h1 class="heading-h1 text-primary">گلاب افتخار</h1>
+          <h4 class="heading-h4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h4>
+          <v-btn color="primary" class="header-btn">
+            <p class="heading-h5 pt-1 px-6 text-white"> ادامه</p>
+          </v-btn>
+        </div>
+      </v-main>
+    </v-layout>
+    <v-row class="py-5">
+      <v-col cols="12" md="6" lg="4" v-for="card in 20" >
+        <ProductCard image="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" :productId="card" :title="`شماره ${card}`" price="3000" caption="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است" />
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+import ProductCard from "@/components/productCard.vue";
 </script>
 
 <style scoped>
