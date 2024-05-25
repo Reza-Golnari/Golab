099624ba1a9927254ab7f07ec0c503e94baaf7bf
Reza <i.am.reza.programmer@gmail.com>
2024-05-26 00:18:11 +0330

    footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,12 @@
         <ProductCard image="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" :productId="card" :title="`شماره ${card}`" price="3000" caption="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است" />
       </v-col>
     </v-row>
+    <div class="bg-background pa-2 d-flex align-center justify-center">
+      <div class="d-flex align-center">
+        <v-icon icon="mdi-phone" color="secondaryDark" />
+        <p class="heading-h5 pt-1">09123456789</p>
+      </div>
+    </div>
   </div>
 </template>
 
