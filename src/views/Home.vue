dc2e252b74e78466227858aa7db9374ae3a44cb3
Reza <i.am.reza.programmer@gmail.com>
2024-05-25 23:42:42 +0330

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <v-layout>
-    <v-app-bar class="bg-black h-auto app-bar">
+    <v-app-bar class=" h-auto app-bar">
       <div class="mx-auto h-100">
-        <img src="/images/logo.jpg" alt="logo" class="logo-image">
+        <img src="/images/logo.png" alt="logo" class="logo-image">
       </div>
     </v-app-bar>
-    <v-main class="pa-5 bg-background d-flex align-center justify-center" min-height="300px" style="margin-top: 80px">
+    <v-main class="pa-5 bg-background d-flex align-center justify-center" min-height="400px" style="margin-top: 80px">
       <div class="d-flex flex-column align-center justify-center text-center ga-3" style="max-width: 600px">
-        <h1 class="heading-h1 text-primary">گلاب کاشان</h1>
+        <h1 class="heading-h1 text-primary">گلاب افتخار</h1>
         <h4 class="heading-h4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</h4>
-        <v-btn color="primary">
+        <v-btn color="primary" class="header-btn">
           <p class="heading-h5 pt-1 px-6 text-white"> ادامه</p>
         </v-btn>
       </div>
@@ -28,5 +28,17 @@
 .logo-image{
   height: 70px;
   object-fit: cover;
+}
+
+.header-btn:hover{
+  background-color: #1fa358 !important;
+}
+
+.header-btn p{
+  transition: color 150ms;
+}
+
+.header-btn:hover p {
+  color: black !important;
 }
 </style>
