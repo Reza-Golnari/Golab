328725e222714bf8ca19fa884058844718abbfd4
Reza <i.am.reza.programmer@gmail.com>
2024-05-25 23:09:41 +0330

    create app bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="h-screen bg-surface">
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <v-layout>
-    <v-app-bar>
-
+    <v-app-bar class="bg-black h-auto app-bar">
+      <div class="mx-auto h-100">
+        <img src="/images/logo.jpg" alt="logo" class="logo-image">
+      </div>
     </v-app-bar>
   </v-layout>
 </template>
@@ -11,5 +13,11 @@
 </script>
 
 <style scoped>
-
+.app-bar{
+  height: 80px !important;
+}
+.logo-image{
+  height: 70px;
+  object-fit: cover;
+}
 </style>
